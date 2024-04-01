2171c3cea20ca9e7d1263af421c3645c27795f45
LazarosZiskos <ziskoslaz@gmail.com>
2024-04-01 19:25:17 +0300

    finished mobile navbar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,10 +13,10 @@ const Sidebar = () => {
       className="sticky top-0 left-0 flex h-screen w-fit flex-col justify-between
   bg-dark-1 p-6 pt-28 text-white max-sm:hidden"
     >
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
           const isActive =
-            pathname === link.route || pathname.startsWith(link.route);
+            pathname === link.route || pathname.startsWith(`${link.route}/`);
 
           return (
             <Link
