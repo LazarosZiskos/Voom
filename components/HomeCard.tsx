e4caaf80769dbf28b6a729900a70101840e652f1
LazarosZiskos <ziskoslaz@gmail.com>
2024-04-02 20:19:58 +0300

    finished with cards on homepage

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,18 +1,26 @@
 "use client";
 import Image from "next/image";
 
-interface PropType {
+interface HomeCardProps {
   icon: string;
   title: string;
   color: string;
+  description: string;
+  handleClick: () => void;
 }
 
-const HomeCard = ({ icon, title, color }: PropType) => {
+const HomeCard = ({
+  icon,
+  title,
+  color,
+  description,
+  handleClick,
+}: HomeCardProps) => {
   return (
     <div
       className={`${color} px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px]
       min-h-[260px] rounded-[14px] cursor-pointer`}
-      onClick={() => {}}
+      onClick={handleClick}
     >
       <div className="flex-center glassmorphism size-12 rounded-[10px]">
         <Image src={icon} alt="meeting-icon" width={27} height={27} />
@@ -20,7 +28,7 @@ const HomeCard = ({ icon, title, color }: PropType) => {
 
       <div className="flex flex-col gap-2">
         <h1 className="text-2xl font-bold">{title}</h1>
-        <p className="text-lg font-normal">Start an instant meeting</p>
+        <p className="text-lg font-normal">{description}</p>
       </div>
     </div>
   );
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,35 +1,44 @@
 "use client";
 
+import { useState } from "react";
 import HomeCard from "./HomeCard";
+import { useRouter } from "next/navigation";
 
-interface PropType {
-  icon: string;
-  title: string;
-  color: string;
-}
+const MeetingTypeList = () => {
+  const router = useRouter();
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >();
 
-const MeetingTypeList = ({ icon, title, color }: PropType) => {
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         icon="/icons/add-meeting.svg"
+        description="Start an instant meeting"
         title="New Metting"
         color="bg-orange-1"
+        handleClick={() => setMeetingState("isInstantMeeting")}
       />
       <HomeCard
         icon="/icons/join-meeting.svg"
+        description="via invitation link"
         title="Join Metting"
         color="bg-blue-1"
+        handleClick={() => setMeetingState("isJoiningMeeting")}
       />
       <HomeCard
         icon="/icons/schedule.svg"
+        description="Plan your meeting"
         title="Schedule Metting"
         color="bg-purple-1"
+        handleClick={() => setMeetingState("isScheduleMeeting")}
       />
       <HomeCard
         icon="/icons/recordings.svg"
+        description="Check out your recordings"
         title="View Recordings"
         color="bg-yellow-1"
+        handleClick={() => router.push("/recordings")}
       />
     </section>
   );
