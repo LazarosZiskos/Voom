0ff5a33fde32034bc9e368974c410b78506198c5
LazarosZiskos <ziskoslaz@gmail.com>
2024-04-04 19:41:48 +0300

    finished developing shedule meeting

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -65,6 +65,8 @@ const MeetingTypeList = () => {
     }
   };
 
+  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetails?.id}`;
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
@@ -122,10 +124,10 @@ const MeetingTypeList = () => {
               selected={values.dateTime}
               onChange={(date) => setValues({ ...values, dateTime: date! })}
               showTimeSelect
-              timeFormat="HH:MM"
+              timeFormat="HH:mm"
               timeIntervals={15}
               timeCaption="time"
-              dateFormat="MMMM d,yyy h:mm aa"
+              dateFormat="MMMM d, yyyy h:mm aa"
               className="w-full rounded bg-dark-3 p-2 focus:outline-none"
             />
           </div>
@@ -136,7 +138,10 @@ const MeetingTypeList = () => {
           onClose={() => setMeetingState(undefined)}
           title="Meeting Created"
           className="text-center"
-          // handleClick={() => navigator.clipboard.writeText(meetingLink)}
+          handleClick={() => {
+            navigator.clipboard.writeText(meetingLink);
+            toast({ title: "Link Copied" });
+          }}
           icon="icons/checked.svg"
           buttonIcon="/icons/copy.svg"
           buttonText="Copy Meeting Link"
